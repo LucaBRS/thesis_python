f9ac2576e948cd91db8239f6a57551be0612edd9
Lbrs <barsottiniluca@gmail.com>
2022-05-17 13:30:59 +0200

    added some calc

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
 from cmath import log, log10, pi, sqrt
 import numpy as np
+from math import ceil
 
+BW = 125*10**3  # in kHz
+NF = 6  # dipende dal dispositivo in uso (hardwere)
+SF = [7,8,9,10,11,12]
 
-BW = 125*10**3 # in kHz
-NF = 6 # dipende dal dispositivo in uso (hardwere)
-SNR_limit = [("SF7",-7.5),("SF8",-10),("SF9",-12.5),("SF10",-15),("SF11",-17.5),("SF12",-20)]
-S = []
-for snr in SNR_limit:
-    S.append( (snr[0],np.round( (-144 + 10*log10(BW) + NF + snr[1]).real , 3 ) ) )
+PL = 16 # payload in byte
+CRC = 1 # used to determine if all the byte are recived correctly "Cyclic Redundancy Ceck" (for LoRa defoult CRC = 1)
+H = 1 # header, explicit H=0; implicit H=1
 
-print(S)
+DE = 1 # low data rate optimize, enabled: DE=1 
+CR = [1,2,3,4] # Coding Rate, defoult is CR = 1
+CR = 1
+
+T_payload = []
+
+for sf in SF:
+    print(sf)
+    T_s = 2**sf / BW # Symbol Time
+    T_payload.append( T_s*(8 + np.maximum(   ceil(((8*PL - 4*sf +28 + 16*CRC -20*H)/(4*(sf - 2*DE)))*(CR +4))  ,0 )) )
+    
+print(T_payload)
--- utils/calcolo.py
+++ utils/calcolo.py
@@ -1,8 +1,40 @@
 from cmath import log, log10, pi, sqrt
 from distutils.log import error
 from itertools import permutations
+from math import ceil
 from msilib.schema import Error
 import numpy as np
+
+from test import T_payload
+
+SNR_limit = [("SF7", -7.5), ("SF8", -10), ("SF9", -12.5),
+                 ("SF10", -15), ("SF11", -17.5), ("SF12", -20)]
+BW = 125*10**3  # in kHz
+NF = 6  # dipende dal dispositivo in uso (hardwere)
+SF = [7,8,9,10,11,12]
+
+def time_on_air(i):
+    PL = 8 # payload in byte
+    CRC = 1 # used to determine if all the byte are recived correctly "Cyclic Redundancy Ceck" (for LoRa defoult CRC = 1)
+    H = 1 # header, explicit H=0; implicit H=1
+    
+    DE = 1 # low data rate optimize, enabled: DE=1 
+    CR = [1,2,3,4] # Coding Rate, defoult is CR = 1
+    CR = 1
+        
+    sf=SF[i]
+    T_s = 2**sf / BW # Symbol Time
+    T_payload = ( T_s*(8 + np.maximum(   ceil(((8*PL - 4*sf +28 + 16*CRC -20*H)/(4*(sf - 2*DE)))*(CR +4))  ,0 )) )
+
+    return T_payload
+
+def sensibility_calculation():
+    S = []
+    for snr in SNR_limit:
+        S.append(
+            (snr[0], np.round((-144 + 10*log10(BW) + NF + snr[1]).real, 3)))
+
+    return S
 
 
 # calcolo della distanza tramite coordinate
@@ -88,17 +120,26 @@ def double_coverage(meters1, meters2):
     sensibilities = []
     sensibilities = sensibility_calculation()
     dic = {"freeSpace": {},
-           "hata": {}}
+           "hata": {},
+           "#_meters_each_sf_hata":{
+               "description":"meters number excluding the meters with previous SF"
+           },
+           "tot_trasmission_time_hata":{
+               "description":"sum of trasmission time for each meters exlcuding the meters with previous SF",
+               "tot_ToA":0
+           }}
     fs_counter = 0
     hata_counter = 0
-
+    previous_sf_meters = 0
+    sf_counter=0
     if(len(meters1) != len(meters2)):
         print("problema con i meters qualcosa non coincide con lunghezza")
         return Error
-
+    
     for sensibility in sensibilities:
         fs_counter = 0
         hata_counter = 0
+        
         for i in range(len(meters1)):
             if(meters1[i]["id"] != meters2[i]["id"]):
                 print("meters non in ordine crescente")
@@ -117,26 +158,22 @@ def double_coverage(meters1, meters2):
                     hata_counter += 1
                 elif (meters1[i]["recPow"]["hata"] < sensibility[1] and meters2[i]["recPow"]["hata"] > sensibility[1]):
                     hata_counter += 1
-
+      
         dic["freeSpace"][sensibility[0]] = np.round(
             (fs_counter/len(meters1))*100.2)
+       
         dic["hata"][sensibility[0]] = np.round(
             (hata_counter/len(meters1))*100, 2)
 
+        dic["#_meters_each_sf_hata"][sensibility[0]] = hata_counter - previous_sf_meters
+        previous_sf_meters = hata_counter
+        
+        dic["tot_trasmission_time_hata"]["tot_ToA"] = dic["tot_trasmission_time_hata"]["tot_ToA"] + dic["#_meters_each_sf_hata"][sensibility[0]] * time_on_air(sf_counter)
+        sf_counter +=1
     return dic
 
 
-def sensibility_calculation():
-    BW = 125*10**3  # in kHz
-    NF = 6  # dipende dal dispositivo in uso (hardwere)
-    SNR_limit = [("SF7", -7.5), ("SF8", -10), ("SF9", -12.5),
-                 ("SF10", -15), ("SF11", -17.5), ("SF12", -20)]
-    S = []
-    for snr in SNR_limit:
-        S.append(
-            (snr[0], np.round((-144 + 10*log10(BW) + NF + snr[1]).real, 3)))
 
-    return S
 
 
 def simple_combination(list):
@@ -150,3 +187,4 @@ def simple_combination(list):
                     permutation.append((site1, site2))
     print(len(permutation))
     return permutation
+
