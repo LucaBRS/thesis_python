d476100a54ace00f2c6d99ffd8b3b99842796c13
Luca Barsottini <luca.barsottini@outlook.it>
2022-06-15 12:05:43 +0200

    add TOA

--- plotting.py
+++ plotting.py
@@ -45,78 +45,118 @@ for i in range(6):
   #  arr_max[4][i] = (findings['ericsson']['max_toa_ericsson']['%_tot']['SF'+str(7+i)])
 
 
-largbar = 0.25
+largbar = 0.26
 X = np.arange(6)
 
+########################
+## COVERAGE #########
+########################
 
-plt.subplot(2,3,1)
+plt.figure()
 
 plt.title('HATA MEDIUM')
-plt.bar(X ,              arr2i[0],  width=largbar,color='y',label="2irg")
-plt.bar(X + largbar,     arrBC[0],  width=largbar,color='b',label="BC")
-plt.bar(X + largbar*2,   arr_min[0],width=largbar,color='g',label="min")
+plt.bar(X ,              arr2i[0],  width=largbar,color='y',label="Choice 2iReteGas")
+plt.bar(X + largbar,     arrBC[0],  width=largbar,color='b',label="Best Coverage Couple")
+plt.bar(X + largbar*2,   arr_min[0],width=largbar,color='g',label="Minimum ToA Couple")
 # plt.bar(X + largbar*3,   arr_max[0],width=largbar,color='r',label="Max")
 plt.yticks(np.arange(stop=110,step=10))
-plt.xticks([(r+ largbar + largbar/2) for r in range(len(arr2i[0]))],sf_name)
-plt.ylabel('Coverage')
+plt.xticks([(r+ largbar ) for r in range(len(arr2i[0]))],sf_name)
+plt.ylabel('Coverage %')
 plt.xlabel('Spreding Factor')
 plt.legend()
 
 
-plt.subplot(2,3,2)
+plt.figure()
 
 plt.title('HATA BIG')
-plt.bar(X ,              arr2i[1],  width=largbar,color='y',label="2irg")
-plt.bar(X + largbar,     arrBC[1],  width=largbar,color='b',label="BC")
-plt.bar(X + largbar*2,   arr_min[1],width=largbar,color='g',label="min")
+plt.bar(X ,              arr2i[1],  width=largbar,color='y',label="Choice 2iReteGas")
+plt.bar(X + largbar,     arrBC[1],  width=largbar,color='b',label="Best Coverage Couple")
+plt.bar(X + largbar*2,   arr_min[1],width=largbar,color='g',label="Minimum ToA Couple")
 # plt.bar(X + largbar*3,   arr_max[1],width=largbar,color='r',label="Max")
 plt.yticks(np.arange(stop=110,step=10))
-plt.xticks([(r+ largbar + largbar/2) for r in range(len(arr2i[0]))],sf_name)
-plt.ylabel('Coverage')
+plt.xticks([(r+ largbar ) for r in range(len(arr2i[0]))],sf_name)
+plt.ylabel('Coverage %')
 plt.xlabel('Spreding Factor')
 plt.legend()
 
 
-plt.subplot(2,3,3)
+# plt.figure()
 
-plt.title('SUI MEDIUM')
-plt.bar(X ,              arr2i[2],  width=largbar,color='y',label="2irg")
-plt.bar(X + largbar,     arrBC[2],  width=largbar,color='b',label="BC")
-plt.bar(X + largbar*2,   arr_min[2],width=largbar,color='g',label="min")
-# plt.bar(X + largbar*3,   arr_max[2],width=largbar,color='r',label="Max")
-plt.yticks(np.arange(stop=110,step=10))
-plt.xticks([(r+ largbar + largbar/2) for r in range(len(arr2i[0]))],sf_name)
-plt.ylabel('Coverage')
-plt.xlabel('Spreding Factor')
-plt.legend()
-
-
-plt.subplot(2,3,4)
-
-plt.title('SUI BIG')
-plt.bar(X ,              arr2i[3],  width=largbar,color='y',label="2irg")
-plt.bar(X + largbar,     arrBC[3],  width=largbar,color='b',label="BC")
-plt.bar(X + largbar*2,   arr_min[3],width=largbar,color='g',label="min")
-# plt.bar(X + largbar*3,   arr_max[3],width=largbar,color='r',label="Max")
-plt.yticks(np.arange(stop=110,step=10))
-plt.xticks([(r+ largbar + largbar/2) for r in range(len(arr2i[0]))],sf_name)
-plt.ylabel('Coverage')
-plt.xlabel('Spreding Factor')
-plt.legend()
+# plt.title('SUI MEDIUM')
+# plt.bar(X ,              arr2i[2],  width=largbar,color='y',label="2irg")
+# plt.bar(X + largbar,     arrBC[2],  width=largbar,color='b',label="BC")
+# plt.bar(X + largbar*2,   arr_min[2],width=largbar,color='g',label="min")
+# # plt.bar(X + largbar*3,   arr_max[2],width=largbar,color='r',label="Max")
+# plt.yticks(np.arange(stop=110,step=10))
+# plt.xticks([(r+ largbar + largbar/2) for r in range(len(arr2i[0]))],sf_name)
+# plt.ylabel('Coverage')
+# plt.xlabel('Spreding Factor')
+# plt.legend()
 
 
-plt.subplot(2,3,5)
+# plt.figure()
+
+# plt.title('SUI BIG')
+# plt.bar(X ,              arr2i[3],  width=largbar,color='y',label="2irg")
+# plt.bar(X + largbar,     arrBC[3],  width=largbar,color='b',label="BC")
+# plt.bar(X + largbar*2,   arr_min[3],width=largbar,color='g',label="min")
+# # plt.bar(X + largbar*3,   arr_max[3],width=largbar,color='r',label="Max")
+# plt.yticks(np.arange(stop=110,step=10))
+# plt.xticks([(r+ largbar + largbar/2) for r in range(len(arr2i[0]))],sf_name)
+# plt.ylabel('Coverage')
+# plt.xlabel('Spreding Factor')
+# plt.legend()
+
+
+plt.figure()
 
 plt.title('ERICSSON')
-plt.bar(X ,              arr2i[4],  width=largbar,color='y',label="2irg")
-plt.bar(X + largbar,     arrBC[4],  width=largbar,color='b',label="BC")
-plt.bar(X + largbar*2,   arr_min[4],width=largbar,color='g',label="min")
+plt.bar(X ,              arr2i[4],  width=largbar,color='y',label="Choice 2iReteGas")
+plt.bar(X + largbar,     arrBC[4],  width=largbar,color='b',label="Best Coverage Couple")
+plt.bar(X + largbar*2,   arr_min[4],width=largbar,color='g',label="Minimum ToA Couple")
 # plt.bar(X + largbar*3,   arr_max[4],width=largbar,color='r',label="Max")
 plt.yticks(np.arange(stop=110,step=10))
-plt.xticks([(r+ largbar + largbar/2) for r in range(len(arr2i[0]))],sf_name)
-plt.ylabel('Coverage')
+plt.xticks([(r + largbar ) for r in range(len(arr2i[0]))],sf_name)
+plt.ylabel('Coverage %')
 plt.xlabel('Spreding Factor')
 plt.legend()
+
+
+################
+## ToA ######
+################
+delay_name = ['Hata medium','Hata big','Ericsson']
+
+ToA_2i = np.zeros((3))
+ToA_BC = np.zeros((3))
+ToA_min = np.zeros((3))
+
+X = np.arange(3)
+
+ToA_2i[0] = (choice2irg['coverage']['hata_medium']['avg_optimize_ToA'])
+ToA_BC[0] = (findings['hata_medium']['bestCoverage_hata_medium']['avg_optimize_ToA'])
+ToA_min[0] = (findings['hata_medium']['min_toa_hata_medium']['avg_optimize_ToA'])
+
+ToA_2i[1]= (choice2irg['coverage']['hata_big']['avg_optimize_ToA'])
+ToA_BC[1] = (findings['hata_big']['bestCoverage_hata_big']['avg_optimize_ToA'])
+ToA_min[1]= (findings['hata_big']['min_toa_hata_big']['avg_optimize_ToA'])
+
+ToA_2i[2] = (choice2irg['coverage']['ericsson']['avg_optimize_ToA'])
+ToA_BC[2] = (findings['ericsson']['bestCoverage_ericsson']['avg_optimize_ToA'])
+ToA_min[2] = (findings['ericsson']['min_toa_ericsson']['avg_optimize_ToA'])
+
+plt.figure()
+
+plt.title('ToA Comparison')
+plt.bar(X ,              ToA_2i,  width=largbar,color='y',label="Choice 2iReteGas")
+plt.bar(X + largbar,     ToA_BC,  width=largbar,color='b',label="Best Coverage Couple")
+plt.bar(X + largbar*2,   ToA_min,width=largbar,color='g',label="Minimum ToA Couple")
+plt.xticks([(r + largbar ) for r in range(len(ToA_2i))],delay_name)
+plt.legend()
+plt.ylabel('Avarage ToA (sec)')
+plt.xlabel('Loss Type')
+
+
 ##########################################################################################
 ##########################################################################################
 
@@ -138,10 +178,10 @@ for meter in distance_power[0]['meters']:
     x[6][index] = meter["recPow"]["ericsson"]
     index +=1
 
-plt.subplot(2,3,6)
+plt.figure()
 plt.title("Recived Power")
 plt.plot(x[0][:],x[1][:],'r', x[0][:],x[2][:],'g', x[0][:],x[3][:],'g--', x[0][:],x[4][:],'b', x[0][:],x[5][:],'b--' , x[0][:],x[6][:],'black' )
-plt.ylabel('decibiel')
+plt.ylabel('Decibiel')
 plt.xlabel('Distance (km)')
 plt.legend(['FreeSpace','Hata (medium)','Hata (big)','SUI (medium)','SUI (big)','Ericsson'])
 plt.show()
